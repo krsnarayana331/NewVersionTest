ef9a27adb16b39dcb7e7332a403a827455d05cf8
Surya <krsnarayana@gmail.com>
2023-03-28 00:04:41 +0530

    This is new commit from new Guy

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,9 +2,13 @@ public class Test {
 
     public static void main(String[] args) {
         mul(4,5);
+        mod(8,2);
     }
 
     public static void mul(int a, int b){
         System.out.println("Mul of Two Integers: "+(a*b));
+    }
+    public static void mod(int a, int b){
+        System.out.println("Mod of Two Integers: "+(a*b));
     }
 }
