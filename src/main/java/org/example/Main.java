61bcd742ec2c4ae61c7ad1d4efdf98c83bf92ab7
Surya <krsnarayana@gmail.com>
2023-03-27 23:04:01 +0530

    This is First Commit By Yahoo email

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        sum(6,5);
+    }
+
+    public static void sum(int a, int b){
+        System.out.println("Sum of Two Integers: "+(a+b));
+    }
+}
