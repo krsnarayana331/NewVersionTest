10834570e8c133fd1ffda528cdf9b86aa634702e
Surya <krsnarayana@gmail.com>
2023-03-27 23:25:56 +0530

    This is 2nd Commit from yahoo email

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+        mul(4,5);
+    }
+
+    public static void mul(int a, int b){
+        System.out.println("Mul of Two Integers: "+(a*b));
+    }
+}
